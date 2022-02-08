79fdb349351896a78892c0425557927a5a817e53
skskdmsdl <skskdmsdl@gmail.com>
2022-02-08 22:57:31 +0900

    life cycle

---
+++ basicSpring/src/main/java/lifeCycle/com/brms/member/service/MemberRegisterService.java
@@ -0,0 +1,18 @@
+package lifeCycle.com.brms.member.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import lifeCycle.com.brms.member.Member;
+import lifeCycle.com.brms.member.dao.MemberDao;
+
+public class MemberRegisterService {
+
+	@Autowired
+	private MemberDao memberDao;
+	
+	public MemberRegisterService() { }
+	
+	public void register(Member member) {
+		memberDao.insert(member);
+	}
+}
