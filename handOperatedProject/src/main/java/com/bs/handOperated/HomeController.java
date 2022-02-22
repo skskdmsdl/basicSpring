e7156e5f50afa88505f1b0873d11d35e6da67481
skskdmsdl <skskdmsdl@gmail.com>
2022-02-22 22:39:20 +0900

    수동으로 스프링  파일 직접 생성

---
+++ handOperatedProject/src/main/java/com/bs/handOperated/HomeController.java
@@ -0,0 +1,30 @@
+package com.bs.handOperated;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home(Model model) {
+		
+		System.out.println("---- home() method ----");
+		
+		model.addAttribute("key", "value");
+		
+		return "home";
+	}
+	
+	@RequestMapping("/login")
+	public String login(Model model) {
+		
+		System.out.println("---- login() method ----");
+		
+		model.addAttribute("key", "value");
+		
+		return "login";
+	}
+
+}
