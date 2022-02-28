48a4be5b2c4bc9f76719b09544d72f0088839ffb
skskdmsdl <skskdmsdl@gmail.com>
2022-02-28 10:58:20 +0900

    session study

---
+++ session/src/main/java/com/bs/session/member/dao/IMemberDao.java
@@ -0,0 +1,12 @@
+package com.bs.session.member.dao;
+
+import java.util.Map;
+
+import com.bs.session.member.Member;
+
+public interface IMemberDao {
+	Map<String, Member> memberInsert(Member member);
+	Member memberSelect(Member member);
+	Member memberUpdate(Member member);
+	Map<String, Member> memberDelete(Member member);
+}
