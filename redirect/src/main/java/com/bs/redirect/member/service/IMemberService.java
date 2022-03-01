cd35445b5fda46b3d71597564de2a8fc526f0aed
skskdmsdl <skskdmsdl@gmail.com>
2022-03-01 18:31:10 +0900

    redirect study

---
+++ redirect/src/main/java/com/bs/redirect/member/service/IMemberService.java
@@ -0,0 +1,10 @@
+package com.bs.redirect.member.service;
+
+import com.bs.redirect.member.Member;
+
+public interface IMemberService {
+	void memberRegister(Member member);
+	Member memberSearch(Member member);
+	Member memberModify(Member member);
+	void memberRemove(Member member);
+}
